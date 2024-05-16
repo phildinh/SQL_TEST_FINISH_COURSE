ae40036f60a415bd1e903da24301ebcf6e8d02bf
phildinh <dinhthanhtrung2011@gmail.com>
2024-05-16 11:58:50 +1000

    Add files via upload

---
+++ QUESTION.sql
@@ -0,0 +1,39 @@
+-- QUESTION 1:
+/* How can you write a SQL query to calculate the total sales of furniture products,
+grouped by each quarter of the year, and order the results chronologically? */
+
+
+-- QUESTION 2:
+/* How can you analyze the impact of different discount levels on sales performance across product categories, 
+specifically looking at the number of orders and total profit generated for each discount classification?
+
+Discount level condition:
+No Discount = 0
+0 < Low Discount < 0.2
+0.2 < Medium Discount < 0.5
+High Discount > 0.5 */
+
+
+-- QUESTION 3:
+/* How can you determine the top-performing product categories within each customer segment based on sales and profit, 
+focusing specifically on those categories that rank within the top two for profitability? */
+
+
+-- QUESTION 4
+/*
+How can you create a report that displays each employee's performance across different product categories, 
+showing not only the total profit per category but also what percentage of 
+their total profit each category represents, with the results ordered by the 
+percentage in descending order for each employee?
+*/
+
+
+-- QUESTION 5:
+/*
+How can you develop a user-defined function in SQL Server 
+to calculate the profitability ratio for each product category an employee has sold, 
+and then apply this function to generate a report that 
+ranks each employee's product categories by their profitability ratio?
+*/
+
+
